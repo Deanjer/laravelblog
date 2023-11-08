92536b345684e713125849d1d05b3adaf69060ec
Deanjer <220509@student.glu.nl>
2023-11-08 14:14:20 +0100

    comment-section-db

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -10,26 +10,56 @@
 </head>
 
 <body>
+    <div class="center">
+        <div class="flex-container">
+            <div class="content-container">
+                <article>
+                    <h1 class="title"><?= $item->title; ?></h1>
+                    <img class="top-image" src="/images/illustration-5.png" alt="Blog Post illustration" class="rounded-xl" />
+                    <div>
+                        <?= $item->info; ?>
+                        <br>
+                        <br>
+                        <?= $item->excerpt; ?>
+                    </div>
 
-    <article>
-        <h1><a href="post/<?= $item->id; ?>"><?= $item->title; ?></a></h1>
-        <div>
-            <?= $item->info; ?>
+                    <div class="comments">
+
+                    </div>
             <br>
-            <br>
-            <?= $item->excerpt; ?>
+                    <a class="back-button" href="/">Go back</a>
+                </article>
+                <div class="comments-container">
+                    <div class="create-comment">
+                        <h2>Add reaction</h2>
+                        <form action="" >
+                            <input type="text" value="<?php $item->id?>">
+                            <label for="name">Naam</label>
+                            <br>
+                            <input name="name" type="text">
+                            <br>
+                            <br>
+                            <label for="comment">Comment</label>
+                            <br>
+                            <textarea name="comment" cols="30" rows="10"></textarea>
+                            <br>
+                            <input type="submit">
+                        </form>
+                    </div>
+                    <br>
+                    <hr>
+                    <div class="comments">
+                        <?php
+                        foreach ($comment_item as $comment) {
+                            // echo $comment->post_id;
+                            echo '<br>' . $comment->comment . '<br><br>';
+                        }
+                        ?>
+                    </div>
+                </div>
+            </div>
         </div>
-    </article>
-    <div class="comments">
-    <?php
-            foreach ($comment_item as $comment) {
-                echo $comment->post_id;
-            }
-            ?>
     </div>
-
-    <a href="/">Go back</a>
 </body>
 
 </html>
-
