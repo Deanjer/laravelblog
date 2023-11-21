00e39fe7ab34ebfa8ae69b230b162eac0ca2371b
Deanjer <220509@student.glu.nl>
2023-11-21 14:12:13 +0100

    Register-login-done

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -82,34 +82,40 @@
 
                 <div class="">
                     <div class="create-comment">
-                        <h2>Add reaction</h2>
+                        <h2 class="mt-5 font-bold">Add reaction</h2>
                         <!-- action=" <?php //echo url('post/'.$item->id); 
                                         ?>"  -->
                         <form method="post">
                             @csrf
-                            <input name="post_id" type="hidden" value="<?= $item->id; ?>">
-                            <label for="name">Naam</label>
+                            <input name="post_id" type="hidden" value=" {{$item->id}}">
+                            
                             <br>
-                            <input name="name" type="text">
+                            <label class="mb-2 text-sm font-medium text-gray-900 dark:text-white" for="">Name</label><br>
+                            <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-1/3 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="name" type="text">
                             <br>
                             <br>
-                            <label for="comment">Comment</label>
+                            <label class="mb-2 text-sm font-medium text-gray-900 dark:text-white" for="comment">Comment</label>
                             <br>
-                            <textarea name="comment" cols="30" rows="10"></textarea>
+                            <textarea class=" p-2.5 w-1/3 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-2" name="comment" cols="30" rows="10"></textarea>
                             <br>
-                            <input class="back-button" type="submit">
+                            <input class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="submit">
                         </form>
                     </div>
 
                     <br>
                     <hr>
-                    <div class="comments">
-                        <?php
-                        foreach ($comment_item as $comment) {
-                            // echo $comment->post_id;
-                            echo '<br><div class="comment-card">' . '<div class="comment-content"><b>' . $comment->name . '</b><br>' . $comment->comment . '</div></div><br><br>';
-                        }
-                        ?>
+                    
+                    <div class="flex justify-center text-left">
+                        <div class="w-2/3">
+                            @foreach($comment_item as $comment)
+
+                            <br>
+                            <div class="border-1">
+                                <div class="border-2 p-3 rounded-xl"><b> {{$comment->name}}</b><br> {{$comment->comment}} </div>
+                            </div>
+
+                            @endforeach
+                        </div>
                     </div>
                 </div>
         </div>
